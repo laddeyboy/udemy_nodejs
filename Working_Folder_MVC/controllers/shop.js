df8d2c08ab4769b69231cb0ab7ff0ee4ca85e343
Josh Ladwig <joshua_ladwig@yahoo.com>
2019-07-29 21:45:10 -0500

    added detailed product functionality

--- Working_Folder_MVC/controllers/shop.js
+++ Working_Folder_MVC/controllers/shop.js
@@ -1,43 +1,65 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('shop/product-list', { 
-      prods: products, 
-      pageTitle: 'All Products',
-      path: '/products',
-     });
-  });  
-}
+  Product.fetchAll(products => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products"
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products"
+    });
+  });
+};
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('shop/index', { 
-      prods: products, 
-      pageTitle: 'Shop',
-      path: '/',
+  Product.fetchAll(products => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
       hasProducts: products.length > 0,
       activeShop: true,
-      productCSS: true});
-  });  
-}
+      productCSS: true
+    });
+  });
+};
 
 exports.getCart = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('shop/cart', { 
-      prods: products, 
-      pageTitle: 'Your Cart',
-      path: '/cart',
-     });
-  });  
-}
+  Product.fetchAll(products => {
+    res.render("shop/cart", {
+      prods: products,
+      pageTitle: "Your Cart",
+      path: "/cart"
+    });
+  });
+};
 
 exports.getCheckout = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('shop/checkout', { 
-      prods: products, 
-      pageTitle: 'Checkout',
-      path: '/checkout',
-      });
-  });  
-}
+  Product.fetchAll(products => {
+    res.render("shop/checkout", {
+      prods: products,
+      pageTitle: "Checkout",
+      path: "/checkout"
+    });
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render("shop/orders", {
+      prods: products,
+      pageTitle: "Your Orders",
+      path: "/orders"
+    });
+  });
+};
--- Working_Folder_MVC/routes/shop.js
+++ Working_Folder_MVC/routes/shop.js
@@ -5,11 +5,14 @@ const express = require('express');
 const router = express.Router();
 
 const shopController = require('../controllers/shop');
-const { getProducts, getCart, getCheckout, getIndex } = shopController;
+const { getProducts, getCart, getCheckout, getIndex, getOrders, getProduct } = shopController;
 
 router.get('/', getIndex);
 router.get('/products', getProducts);
+// ':' signals to express that productId is a variable name to be extracted.
+router.get('/products/:productId', getProduct );
 router.get('/cart', getCart);
+router.get('/orders', getOrders);
 router.get('/checkout', getCheckout);
 
 
