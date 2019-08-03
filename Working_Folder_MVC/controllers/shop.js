d9cf0a56898ee9da552348bc270243b3a1e2aa8e
Josh Ladwig <joshua_ladwig@yahoo.com>
2019-08-03 08:06:22 -0500

    added Add To Cart post routing

--- Working_Folder_MVC/controllers/shop.js
+++ Working_Folder_MVC/controllers/shop.js
@@ -44,6 +44,12 @@ exports.getCart = (req, res, next) => {
   });
 };
 
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect("/cart");
+};
+
 exports.getCheckout = (req, res, next) => {
   Product.fetchAll(products => {
     res.render("shop/checkout", {
--- Working_Folder_MVC/routes/shop.js
+++ Working_Folder_MVC/routes/shop.js
@@ -1,20 +1,27 @@
 // the route of what the user sees -> this is what we're going to be building over the course
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const shopController = require('../controllers/shop');
-const { getProducts, getCart, getCheckout, getIndex, getOrders, getProduct } = shopController;
+const shopController = require("../controllers/shop");
+const {
+  getProducts,
+  getCart,
+  postCart,
+  getCheckout,
+  getIndex,
+  getOrders,
+  getProduct
+} = shopController;
 
-router.get('/', getIndex);
-router.get('/products', getProducts);
+router.get("/", getIndex);
+router.get("/products", getProducts);
 // ':' signals to express that productId is a variable name to be extracted.
-router.get('/products/:productId', getProduct );
-router.get('/cart', getCart);
-router.get('/orders', getOrders);
-router.get('/checkout', getCheckout);
-
-
+router.get("/products/:productId", getProduct);
+router.get("/cart", getCart);
+router.post("/cart", postCart);
+router.get("/orders", getOrders);
+router.get("/checkout", getCheckout);
 
 module.exports = router;
