ec702d8af4e2734d2eb152614dbad94e48ff327c
Josh Ladwig <joshua_ladwig@yahoo.com>
2019-08-08 10:01:32 -0500

    added functionality for editing/updating products

--- Working_Folder_MVC/controllers/admin.js
+++ Working_Folder_MVC/controllers/admin.js
@@ -1,33 +1,73 @@
-const Product = require('../models/product');
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+  res.render("admin/edit-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    editing: false
   });
-}
+};
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
-  res.redirect('/');
-}
+  res.redirect("/");
+};
+
+exports.getEditProduct = (req, res, next) => {
+  // getting a query parameter
+  const editMode = req.query.edit;
+  // getting a passed parameter
+  const prodId = req.params.productId;
+  if (!editMode) {
+    return res.redirect("/");
+  }
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.redirect("/");
+    }
+    console.log("what is editMode:", editMode);
+    res.render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: editMode,
+      product: product
+    });
+  });
+};
+
+exports.postEditProduct = (req, res, next) => {
+  // fetch information for the product
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updateImageUrl = req.body.imageUrl;
+  const updatedDescription = req.body.description;
+  // create new Product instance and call save
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updateImageUrl,
+    updatedDescription,
+    updatedPrice
+  );
+  updatedProduct.save();
+  res.redirect("/admin/products");
+};
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render('admin/products', { 
-      prods: products, 
-      pageTitle: 'Admin Products',
-      path: '/admin/products',
+  Product.fetchAll(products => {
+    res.render("admin/products", {
+      prods: products,
+      pageTitle: "Admin Products",
+      path: "/admin/products",
       hasProducts: products.length > 0,
       activeShop: true,
-      productCSS: true});
-  });  
-}
+      productCSS: true
+    });
+  });
+};
--- Working_Folder_MVC/routes/admin.js
+++ Working_Folder_MVC/routes/admin.js
@@ -1,15 +1,22 @@
 // this should be the route that handles the creation of products which admin can use
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const adminController = require('../controllers/admin');
-const { getAddProduct, postAddProduct, getProducts } = adminController;
+const adminController = require("../controllers/admin");
+const {
+  getAddProduct,
+  postAddProduct,
+  getProducts,
+  getEditProduct,
+  postEditProduct
+} = adminController;
 
-router.get('/add-product', getAddProduct);
-router.post('/add-product', postAddProduct );
-
-router.get('/products', getProducts);
+router.get("/add-product", getAddProduct);
+router.post("/add-product", postAddProduct);
+router.get("/products", getProducts);
+router.get("/edit-product/:productId", getEditProduct);
+router.post("/edit-product/", postEditProduct);
 
 module.exports = router;
