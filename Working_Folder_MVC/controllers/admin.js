9ecc412b006ed5adec56873ecb32bc8756cb6125
Josh Ladwig <joshua_ladwig@yahoo.com>
2019-08-26 14:30:21 -0500

    added functionality for using squelize to cart

--- Working_Folder_MVC/controllers/admin.js
+++ Working_Folder_MVC/controllers/admin.js
@@ -13,13 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageurl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title,
-    price,
-    imageurl,
-    description
-  })
-    .then(result => console.log("Created Product"))
+  req.user
+    .createProduct({
+      title,
+      price,
+      imageurl,
+      description
+    })
+    .then(result => {
+      console.log("CREATED PRODUCT");
+      res.redirect("/admin/products");
+    })
     .catch(err => console.log(err));
   // pre Sequelize
   // const product = new Product(null, title, imageUrl, description, price);
@@ -36,22 +40,40 @@ exports.postAddProduct = (req, res, next) => {
 exports.getEditProduct = (req, res, next) => {
   // getting a query parameter
   const editMode = req.query.edit;
-  // getting a passed parameter
-  const prodId = req.params.productId;
   if (!editMode) {
     return res.redirect("/");
   }
-  Product.findById(prodId, product => {
-    if (!product) {
-      return res.redirect("/");
-    }
-    res.render("admin/edit-product", {
-      pageTitle: "Edit Product",
-      path: "/admin/edit-product",
-      editing: editMode,
-      product: product
-    });
-  });
+  // getting a passed parameter
+  const prodId = req.params.productId;
+  req.user
+    .getProducts({ where: { id: prodId } })
+    // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect("/");
+      }
+      res.render("admin/edit-product", {
+        pageTitle: "Edit Product",
+        path: "/admin/edit-product",
+        editing: editMode,
+        product: product
+      });
+    })
+    .catch(err => console.log("[admin.js] getEditProducts", err));
+
+  //This code if pre-sequelize
+  //   product => {
+  //   if (!product) {
+  //     return res.redirect("/");
+  //   }
+  //   res.render("admin/edit-product", {
+  //     pageTitle: "Edit Product",
+  //     path: "/admin/edit-product",
+  //     editing: editMode,
+  //     product: product
+  //   });
+  // });
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -62,19 +84,36 @@ exports.postEditProduct = (req, res, next) => {
   const updateImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
   // create new Product instance and call save
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updateImageUrl,
-    updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      product.imageurl = updateImageUrl;
+      return product.save();
+    })
+    .then(result => {
+      console.log("UPDATED PRODUCT");
+      res.redirect("/admin/products");
+    })
+    .catch(err => console.log("[admin.js] postEditProduct", err));
+
+  // code for pre-Sequelize
+  // const updatedProduct = new Product(
+  //   prodId,
+  //   updatedTitle,
+  //   updateImageUrl,
+  //   updatedDescription,
+  //   updatedPrice
+  // );
+  // updatedProduct.save();
+  // res.redirect("/admin/products");
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts()
+    // Product.findAll()
     .then(products => {
       res.render("admin/products", {
         prods: products,
@@ -98,6 +137,16 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log("DESTROYED PRODUCT");
+      res.redirect("/admin/products");
+    })
+    .catch(err => console.log("[admin.js] postDeleteProduct", err));
+  // code pre-Sequelize
+  // Product.deleteById(prodId);
+  // res.redirect("/admin/products");
 };
--- Working_Folder_MVC/server.js
+++ Working_Folder_MVC/server.js
@@ -8,8 +8,12 @@ const errorController = require("./controllers/error");
 // this is the 'pool'
 // const db = require("./util/database");
 const sequelize = require("./util/database");
-const { get404 } = errorController;
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
+const { get404 } = errorController;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -23,17 +27,46 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public"))); // this is granting access to read only access to these files.
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then(user => {
+      // this is a sequelized object NOT a standard JS object
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 // adminRoutes is the exported object from admin.js
 app.use("/admin", adminRoutes); // the first parameter becomes a filter so only paths that start with '/admin'
 // will be handled here.
 app.use(shopRoutes);
-
 app.use(get404);
 
 // this syncs my sequelize models to the database by creating appropriate tables.
+// association my models together check out: https://sequelize.org/master/manual/associations.html
+
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); // this is the 'user' created the 'product'
+User.hasMany(Product); // this is optional and shows 'bi-directionality'
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then(result => {
-    app.listen(3000);
+    // create a user
+    return User.findByPk(1);
   })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: "Josh", email: "test@test.com" });
+    }
+    return Promise.resolve(user);
+  })
+  .then(user => {
+    return user.createCart();
+  })
+  .then(cart => app.listen(3000))
   .catch(err => console.log(`Sync Error ${err}`));
