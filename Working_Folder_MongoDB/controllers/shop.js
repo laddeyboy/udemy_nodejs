b3a8747b79034419299597a447ee8b68c3f91a3f
Josh Ladwig <joshua_ladwig@yahoo.com>
2019-09-13 10:17:43 -0500

    completed app conversion to MongoDB

--- Working_Folder_MongoDB/controllers/shop.js
+++ Working_Folder_MongoDB/controllers/shop.js
@@ -78,84 +78,23 @@ exports.postCart = (req, res, next) => {
     .then(result => {
       res.redirect("/cart");
     });
-
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: prodId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) {
-  //       product = products[0];
-  //     }
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(prodId);
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity }
-  //     });
-  //   })
-  //   .then(() => {
-  //     res.redirect("/cart");
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: prodId } });
-    })
-    .then(products => {
-      const product = products[0];
-      return product.cartItem.destroy();
-    })
+    .deleteItemFromCart(prodId)
     .then(result => {
       res.redirect("/cart");
     })
     .catch(err => console.log("[shop.js]postCartDeleteProduct", err));
-  // Product.findById(prodId, product => {
-  //   Cart.deleteProduct(prodId, product.price);
-  //   res.redirect("/cart");
-  // });
 };
 
 exports.postOrder = (req, res, next) => {
   // take all cart items and move them to order
   let fetchedCart;
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity };
-              return product;
-            })
-          );
-        })
-        .catch(err => console.log(err));
-    })
-    .then(result => {
-      return fetchedCart.setProducts(null);
-    })
+    .addOrder()
     .then(result => {
       res.redirect("/orders");
     })
@@ -164,7 +103,7 @@ exports.postOrder = (req, res, next) => {
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ["products"] })
+    .getOrders()
     .then(orders => {
       res.render("shop/orders", {
         path: "/orders",
--- Working_Folder_MongoDB/routes/shop.js
+++ Working_Folder_MongoDB/routes/shop.js
@@ -19,12 +19,11 @@ const {
 
 router.get("/", getIndex);
 router.get("/products", getProducts);
-// ':' signals to express that productId is a variable name to be extracted.
 router.get("/products/:productId", getProduct);
 router.get("/cart", getCart);
 router.post("/cart", postCart);
-// router.post("/cart-delete-item", postCartDeleteProduct);
-// router.get("/orders", getOrders);
-// router.post("/create-order", postOrder);
+router.post("/cart-delete-item", postCartDeleteProduct);
+router.get("/orders", getOrders);
+router.post("/create-order", postOrder);
 
 module.exports = router;
